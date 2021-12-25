55cd29592a3df37e87d3abc9552b4c75eef1c86e
lebrancconvas <lebrancconvas@gmail.com>
2021-12-25 23:38:30 +0700

    👔 Create Todo Adding Feature.

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,13 +1,28 @@
-import React, {useState} from 'react'; 
+import React, {SyntheticEvent, useState} from 'react'; 
 
 const Homepage = () => {
-	const [todo, setTodo] = useState(''); 
+	const [todo, setTodo] = useState('');  
+	// const [value, setValue] = useState(''); 
+	const todoList: string[] = []; 
+	const addTodo = (e: SyntheticEvent) => {
+		todoList.push(todo);   	
+		// setTodo(e.target.value);         
+	}; 
+
+	console.log(`To-Do-List: ${todoList}`); 
 	return (
 		<div>
 			<h1>Perfect To-Do-List</h1> 
-			<form>
-				<input type="text" placeholder="Add Task" />
-				<button type="submit">
+			<ol>
+				{todoList.map((list, index) => (
+					<li key={index}> 
+						{list} 
+					</li>
+				))} 
+			</ol>
+			<form onSubmit={addTodo}>
+				<input type="text" placeholder="Add Task" name="Todo" onChange={e => setTodo(e.target.value)}  />  
+				<button type="submit"> 
 					Add Todo 
 				</button>
 			</form>
